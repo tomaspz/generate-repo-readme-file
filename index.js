fe0a7c1c5bc8777bff0942f55555c4f2d6d4de75
Tomas Perez <tomaspz@gmail.com>
2020-03-25 19:37:13 -0400

    created the inquirer questions with defaults and choices

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
 
-const questions = ["What is your GitHub username?: ", "What label would you like for your badge?: ", "What message would you like for your badge?: ", "What is the project title?: ", "Please, provide a description of your project: ", "Provide the table of contents: ", "Provide the installation instructions: ", "Provide the usage: ", "What type of license are you using?: ", "Please, list the contributors to the project: ", "Provide information about the tests: ", "Do you want to add any questions?: "
+const questions = [
+    "What is your GitHub username?: ", 
+    "What type of license are you using?: ", 
+    "What is the project title?: ", 
+    "Please, provide a description of your project: ", 
+    "Provide the table of contents: ", 
+    "Provide the installation instructions: ", 
+    "Provide the usage: ",  
+    "Please, list the contributors to the project: ", 
+    "Provide information about the tests: ", 
+    "Do you want to add any questions?: "
 ];
 
 inquirer.prompt([
     {type: "input", message: questions[0], name: "userName"},
-    {type: "input", message: questions[1], name: "badgeLabel"},
-    {type: "input", message: questions[2], name: "badgeMessage" },
-    {type: "input", message: questions[3], name: "projectTitle" },
-    {type: "input", message: questions[4], name: "projectDescription" },
-    {type: "input", message: questions[5], name: "tableContents" },
-    {type: "input", message: questions[6], name: "installationInstructions" },
-    {type: "input", message: questions[7], name: "usage" },
-    {type: "input", message: questions[8], name: "license" },
-    {type: "input", message: questions[9], name: "contributors" },
-    {type: "input", message: questions[10], name: "tests" },
-    {type: "input", message: questions[11], name: "questions" }
-    // {type: "checkbox", message: "What technologies do you know?", name: "stack", choices: ["HTML", "CSS", "Javascript", "jQuery"]},
-    // {type: "input", message: "What languages do you know?", name: "languages"},
-    // {type: "input", message: "What is your preferred method of communciation?", name: "communication", choices: ["email", "phone", "telepathy"]}
+    {type: "list", message: questions[1], name: "license", choices: ["MIT", "Public", "GNU", "Copyleft", "Proprietary"]},
+    {type: "input", message: questions[2], name: "projectTitle" },
+    {type: "input", message: questions[3], name: "projectDescription" },
+    {type: "input", message: questions[4], name: "tableContents", default: "Project Title Description Installation Usage Contributors Tests Questions"},
+    {type: "input", message: questions[5], name: "installationInstructions", default: "Run npm install" },
+    {type: "input", message: questions[6], name: "usage", default: "Follow instructions" },
+    {type: "input", message: questions[7], name: "contributors", default: "No contributors" },
+    {type: "input", message: questions[8], name: "tests", default: "No tests" },
+    {type: "input", message: questions[9], name: "questions", default: "No questions" }
   ])
-  .then(function(response) {
-
-    console.log("Your name is: " + response.userName);
-    console.log("The badge will read: " + response.badgeLabel + " " + response.badgeMessage);
-    console.log("The project title: " + response.projectTitle);
-    console.log("The project description: " + response.projectDescription);
+  .then(function(answer) {
+    console.log(answer);
+    // `https://img.shields.io/static/v1?label=License&message=${license}&color=brightgreen`
+  })
+  .catch(err => {
+      if(err) throw err;
+      console.log("No errors in the answers")
   });
 
 
-function writeToFile(fileName, data) {
-    filename = response.name.toLowerCase().split(" ").join("") + ".json";
-    fs.writeFile(filename, JSON.stringify(response, null, "\t"), function(err){
+function writeToFile(fileName, answer) {
+    fileName = answer.name.toLowerCase().split(" ").join("") + ".json";
+    fs.writeFile(fileName, JSON.stringify(answer, null, "\t"), function(err){
         if(err) {
             return err;
         }

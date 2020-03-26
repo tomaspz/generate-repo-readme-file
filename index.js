83bc36a34e5ffcf54aed4b4fd1f0b693efd28f9b
Tomas Perez <tomaspz@gmail.com>
2020-03-25 21:20:54 -0400

    add axios call to get user email and avatar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var fs = require("fs");
-var inquirer = require("inquirer");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
 
 const questions = [
     "What is your GitHub username?: ", 
@@ -19,7 +20,7 @@ inquirer.prompt([
     {type: "list", message: questions[1], name: "license", choices: ["MIT", "Public", "GNU", "Copyleft", "Proprietary"]},
     {type: "input", message: questions[2], name: "projectTitle" },
     {type: "input", message: questions[3], name: "projectDescription" },
-    {type: "input", message: questions[4], name: "tableContents", default: "Project Title Description Installation Usage Contributors Tests Questions"},
+    {type: "input", message: questions[4], name: "tableContents", default: `*Project Title\n *Description\n *Installation\n *Usage\n *Contributors\n *Tests\n *Questions`},
     {type: "input", message: questions[5], name: "installationInstructions", default: "Run npm install" },
     {type: "input", message: questions[6], name: "usage", default: "Follow instructions" },
     {type: "input", message: questions[7], name: "contributors", default: "No contributors" },
@@ -27,31 +28,44 @@ inquirer.prompt([
     {type: "input", message: questions[9], name: "questions", default: "No questions" }
   ])
   .then(answer => {
+
+    axios
+    .get(`https://api.github.com/users/${answer.userName}`)
+    .then(function(response) {
+      console.log(response.data);
       const readmeFileText = `
-        # Project Title: \n
-        ${answer.projectTitle}  \n
-        ![License Badge](https://img.shields.io/static/v1?label=License&message=${answer.license}&color=brightgreen) \n
-        ### Project Description: \n
-        ${answer.projectDescription} \n
-        ### Table of Contents:\n 
-        ${answer.tableContents} \n
-        ### Installation Instructions: \n
-        ${answer.installationInstructions} \n
-        ### Usage: \n
-        ${answer.usage} \n
-        ### Contributors: \n
-        ${answer.contributors} \n
-        ### Tests: \n
-        ${answer.tests} \n
-        ### Questions: \n
-        ${answer.questions}
-        `;
-        fs.appendFile("README.md", readmeFileText, err => {
-            if (err) {
-              return console.log(err);
-            }
-            console.log("The README.md file has been written!");
-        })
+      #Project Title: \n
+      ${answer.projectTitle}  \n
+      https://img.shields.io/static/v1?label=License&message=${answer.license}&color=brightgreen \n
+      ###Project Description: \n
+      ${answer.projectDescription} \n
+      ###Table of Contents:\n 
+      ${answer.tableContents} \n
+      ###Installation Instructions: \n
+      ${answer.installationInstructions} \n
+      ###Usage: \n
+      ${answer.usage} \n
+      ###Contributors: \n
+      ${answer.contributors} \n
+      ###Tests: \n
+      ${answer.tests} \n
+      ###Questions: \n
+      ${answer.questions} \n
+      ###Author Email: \n
+      ${response.data.email} \n
+      ###Author Picture: \n
+      ${response.data.avatar_url}
+      `;
+      fs.appendFile("README.md", readmeFileText, err => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("The README.md file has been written!");
+      })
+    })
+    .catch(function(err){
+      console.log(err);
+    });
   })
   .catch(err => {
       if(err) throw err;
@@ -59,23 +73,23 @@ inquirer.prompt([
   });
 
 
-function writeToFile(fileName, data) {
-    fileName = data.name.toLowerCase().split(" ").join("") + ".json";
-    fs.writeFile(fileName, JSON.stringify(data, null, "\t"), function(err){
-        if(err) {
-            return err;
-        }
-        else {
-            console.log("Success!");
-        }
-    })
-}
+// function writeToFile(fileName, data) {
+//     fileName = data.name.toLowerCase().split(" ").join("") + ".json";
+//     fs.writeFile(fileName, JSON.stringify(data, null, "\t"), function(err){
+//         if(err) {
+//             return err;
+//         }
+//         else {
+//             console.log("Success!");
+//         }
+//     })
+// }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+// init();
 
 
 
